d0a90006209b52220b7fcff5b1b0771f05b2efd4
Elizabeth Harper <foxcapade@gmail.com>
2020-05-13 11:15:31 -0400

    gradle adjustment

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
   ))
 
 
-  implementation(findProject(":core") as Any? ?: "org.veupathdb.lib:jaxrs-container-core:1.0.1")
+  implementation((findProject(":core") as Any? ?: "org.veupathdb.lib:jaxrs-container-core:1.0.1" as Any?)!!)
 
 
   // Jersey
