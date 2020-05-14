aeef2cfdd21f63b428657432b607cb433304bd21
Elizabeth Harper <foxcapade@gmail.com>
2020-05-14 11:47:51 -0400

    bump dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
   ))
 
 
-  implementation(findProject(":core") ?: "org.veupathdb.lib:jaxrs-container-core:1.0.6")
+  implementation(findProject(":core") ?: "org.veupathdb.lib:jaxrs-container-core:1.0.7")
 
 
   // Jersey
