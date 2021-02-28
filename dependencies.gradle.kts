463e84b5c424f665b9e3477f986185c6d7a82e74
Ryan Doherty <rdoherty@upenn.edu>
2021-02-28 13:29:16 -0500

    upgrade core to 5.1

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,7 @@
 //
 // Version Numbers
 //
-val coreLib   = "4.3.0" // Container core lib version
+val coreLib   = "5.1.0" // Container core lib version
 val edaCommon = "1.1.0" // EDA Common version
 
 val jersey  = "2.+"   // Jersey/JaxRS version
