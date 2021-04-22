af23e4a8fcce6a70a9f304ad15373ccd3cac1b2f
Danielle Callan <dcallan@upenn.edu>
2021-04-22 08:51:37 -0400

    bug checking histo viewport is null

--- src/main/java/org/veupathdb/service/eda/ds/plugin/pass/HistogramPlugin.java
+++ src/main/java/org/veupathdb/service/eda/ds/plugin/pass/HistogramPlugin.java
@@ -114,7 +114,7 @@ public class HistogramPlugin extends AbstractPlugin<HistogramPostRequest, Histog
             + ", '" + facetType1 + "'"
             + ", '" + facetType2 + "'), stringsAsFactors=FALSE)");
       
-      if (spec.getViewport().getXMin() != null & spec.getViewport().getXMax() != null) {
+      if (spec.getViewport() != null) {
         connection.voidEval("viewport <- list('xMin'=" + spec.getViewport().getXMin() + ", 'xMax'=" + spec.getViewport().getXMax() + ")");
       } else {
         connection.voidEval("viewport <- NULL");
