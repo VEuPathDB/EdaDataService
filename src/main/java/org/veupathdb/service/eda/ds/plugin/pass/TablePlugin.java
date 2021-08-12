a2aca256402eff949bb3acd2b37d71ffe0fbb9c0
Danielle Callan <dcallan@upenn.edu>
2021-08-11 21:31:09 -0400

    forgot to import OutputStreamWriter

--- src/main/java/org/veupathdb/service/eda/ds/plugin/pass/TablePlugin.java
+++ src/main/java/org/veupathdb/service/eda/ds/plugin/pass/TablePlugin.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
