b141d31088f561d7b0a18d5f27ebd5e43c5b1197
Danielle Callan <dcallan@upenn.edu>
2022-02-11 11:12:49 -0500

    pie plot endpoint first commit

---
+++ src/main/java/org/veupathdb/service/eda/ds/plugin/pass/PieplotPlugin.java
@@ -0,0 +1,102 @@
+package org.veupathdb.service.eda.ds.plugin.pass;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import org.gusdb.fgputil.ListBuilder;
+import org.gusdb.fgputil.validation.ValidationException;
+import org.json.JSONObject;
+import org.veupathdb.service.eda.common.client.spec.StreamSpec;
+import org.veupathdb.service.eda.ds.constraints.ConstraintSpec;
+import org.veupathdb.service.eda.ds.constraints.DataElementSet;
+import org.veupathdb.service.eda.ds.plugin.AbstractPlugin;
+import org.veupathdb.service.eda.generated.model.PieplotPostRequest;
+import org.veupathdb.service.eda.generated.model.PieplotSpec;
+import org.veupathdb.service.eda.generated.model.VariableSpec;
+
+import static org.veupathdb.service.eda.ds.metadata.AppsMetadata.CLINEPI_PROJECT;
+import static org.veupathdb.service.eda.ds.metadata.AppsMetadata.MICROBIOME_PROJECT;
+import static org.veupathdb.service.eda.ds.util.RServeClient.streamResult;
+import static org.veupathdb.service.eda.ds.util.RServeClient.useRConnectionWithRemoteFiles;
+
+public class PieplotPlugin extends AbstractPlugin<PieplotPostRequest, PieplotSpec> {
+
+  @Override
+  public String getDisplayName() {
+    return "Pie plot";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Visualize part-to-whole relationships across the categories of a variable";
+  }
+  
+  @Override
+  public List<String> getProjects() {
+    return List.of(CLINEPI_PROJECT, MICROBIOME_PROJECT);
+  }
+  
+  @Override
+  protected Class<PieplotSpec> getVisualizationSpecClass() {
+    return PieplotSpec.class;
+  }
+
+  @Override
+  public ConstraintSpec getConstraintSpec() {
+    return new ConstraintSpec()
+      .dependencyOrder("xAxisVariable", "facetVariable")
+      .pattern()
+        .element("xAxisVariable")
+          .maxValues(8)
+          .description("Variable must have 8 or fewer unique values.")
+        .element("facetVariable")
+          .required(false)
+          .maxVars(2)
+          .description("Variable(s) must be of the same or a parent entity of the main variable.")
+      .done();
+  }
+
+  @Override
+  protected void validateVisualizationSpec(PieplotSpec pluginSpec) throws ValidationException {
+    validateInputs(new DataElementSet()
+      .entity(pluginSpec.getOutputEntityId())
+      .var("xAxisVariable", pluginSpec.getXAxisVariable())
+      .var("facetVariable", pluginSpec.getFacetVariable()));
+  }
+
+  @Override
+  protected List<StreamSpec> getRequestedStreams(PieplotSpec pluginSpec) {
+    return ListBuilder.asList(
+      new StreamSpec(DEFAULT_SINGLE_STREAM_NAME, pluginSpec.getOutputEntityId())
+        .addVar(pluginSpec.getXAxisVariable())
+        .addVars(pluginSpec.getFacetVariable()));
+  }
+
+  @Override
+  protected void writeResults(OutputStream out, Map<String, InputStream> dataStreams) throws IOException {
+    PieplotSpec spec = getPluginSpec();
+    String showMissingness = spec.getShowMissingness() != null ? spec.getShowMissingness().getValue() : "FALSE";
+
+    Map<String, VariableSpec> varMap = new HashMap<String, VariableSpec>();
+    varMap.put("xAxisVariable", spec.getXAxisVariable());
+    varMap.put("facetVariable1", getVariableSpecFromList(spec.getFacetVariable(), 0));
+    varMap.put("facetVariable2", getVariableSpecFromList(spec.getFacetVariable(), 1));
+      
+    useRConnectionWithRemoteFiles(dataStreams, connection -> {
+      connection.voidEval(getVoidEvalFreadCommand(DEFAULT_SINGLE_STREAM_NAME, 
+          spec.getXAxisVariable(),
+          getVariableSpecFromList(spec.getFacetVariable(), 0),
+          getVariableSpecFromList(spec.getFacetVariable(), 1)));
+      connection.voidEval(getVoidEvalVarMetadataMap(DEFAULT_SINGLE_STREAM_NAME, varMap));
+      String cmd =
+          "plot.data::bar(" + DEFAULT_SINGLE_STREAM_NAME + ", map, " +
+              "'count', 'group', " +
+              showMissingness + ")";
+      streamResult(connection, cmd, out);
+    });
+  }
+}
