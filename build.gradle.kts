1cbbdf1907be51a99ce20f6a0d6fed9ded357cc3
Elizabeth <foxcapade@gmail.com>
2022-07-12 14:02:42 -0400

    update gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.veupathdb.lib.gradle.container.util.Logger.Level
 
 plugins {
   java
-  id("org.veupathdb.lib.gradle.container.container-utils") version "3.4.3"
+  id("org.veupathdb.lib.gradle.container.container-utils") version "4.0.0"
   id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
