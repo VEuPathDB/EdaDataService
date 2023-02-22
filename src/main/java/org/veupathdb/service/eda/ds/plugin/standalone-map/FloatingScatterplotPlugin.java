6c763d2eb233f72e5f44c9b542a0c4716a622c30
Danielle Callan <dcallan@upenn.edu>
2023-02-22 15:25:13 -0500

    draft floating scatter plot

--- src/main/java/org/veupathdb/service/eda/ds/plugin/standalone-map/FloatingScatterplotPlugin.java
+++ src/main/java/org/veupathdb/service/eda/ds/plugin/standalone-map/FloatingScatterplotPlugin.java
@@ -23,11 +23,11 @@ import java.util.Map;
 
 import static org.veupathdb.service.eda.common.plugin.util.RServeClient.streamResult;
 import static org.veupathdb.service.eda.common.plugin.util.RServeClient.useRConnectionWithProcessedRemoteFiles;
-import static org.veupathdb.service.eda.ds.metadata.AppsMetadata.CLINEPI_PROJECT;
+import static org.veupathdb.service.eda.ds.metadata.AppsMetadata.VECTORBASE_PROJECT;
 
-public class ScatterplotPlugin extends AbstractEmptyComputePlugin<ScatterplotPostRequest, ScatterplotSpec> {
+public class FloatingScatterplotPlugin extends AbstractEmptyComputePlugin<FloatingScatterplotPostRequest, FloatingScatterplotSpec> {
 
-  private static final Logger LOG = LogManager.getLogger(ScatterplotPlugin.class);
+  private static final Logger LOG = LogManager.getLogger(FloatingScatterplotPlugin.class);
   
   @Override
   public String getDisplayName() {
@@ -41,93 +41,65 @@ public class ScatterplotPlugin extends AbstractEmptyComputePlugin<ScatterplotPos
 
   @Override
   public List<String> getProjects() {
-    return List.of(CLINEPI_PROJECT);
+    return List.of(VECTORBASE_PROJECT);
   }
 
   @Override
-  protected Class<ScatterplotPostRequest> getVisualizationRequestClass() {
-    return ScatterplotPostRequest.class;
+  protected Class<FloatingScatterplotPostRequest> getVisualizationRequestClass() {
+    return FloatingScatterplotPostRequest.class;
   }
 
   @Override
-  protected Class<ScatterplotSpec> getVisualizationSpecClass() {
-    return ScatterplotSpec.class;
+  protected Class<FloatingScatterplotSpec> getVisualizationSpecClass() {
+    return FloatingScatterplotSpec.class;
   }
 
   @Override
   public ConstraintSpec getConstraintSpec() {
     return new ConstraintSpec()
-      .dependencyOrder(List.of("yAxisVariable", "xAxisVariable"), List.of("overlayVariable", "facetVariable"))
+      .dependencyOrder(List.of("yAxisVariable", "xAxisVariable"), List.of("overlayVariable"))
       .pattern()
         .element("yAxisVariable")
           .types(APIVariableType.NUMBER, APIVariableType.DATE, APIVariableType.INTEGER) 
-          .description("Variable must be a number or date.")
+          .description("Variable must be a number or date and be of the same or a child entity as the X-axis variable.")
         .element("xAxisVariable")
           .types(APIVariableType.NUMBER, APIVariableType.DATE, APIVariableType.INTEGER)
-          .description("Variable must be a number or date and be of the same or a parent entity as the Y-axis variable.")
+          .description("Variable must be a number or date and be the same or a child entity as the variable the map markers are painted with.")
         .element("overlayVariable")
           .required(false)
-          .maxValues(8)
-          .description("Variable must be a number, or have 8 or fewer values, and be of the same or a parent entity as the X-axis variable.")
-        .element("facetVariable")
-          .required(false)
-          .maxVars(2)
-          .maxValues(7)
-          .description("Variable(s) must have 7 or fewer unique values and be of the same or a parent entity as the Overlay variable.")
-      .pattern()
-        .element("yAxisVariable")
-          .types(APIVariableType.NUMBER, APIVariableType.DATE, APIVariableType.INTEGER) 
-          .description("Variable must be a number or date.")
-        .element("xAxisVariable")
-          .types(APIVariableType.NUMBER, APIVariableType.DATE, APIVariableType.INTEGER)
-          .description("Variable must be a number or date and be of the same or a parent entity as the Y-axis variable.")
-        .element("overlayVariable")
-          .types(APIVariableType.NUMBER, APIVariableType.INTEGER) 
-          .description("Variable must be a number, or have 8 or fewer values, and be of the same or a parent entity as the X-axis variable.")
-        .element("facetVariable")
-          .required(false)
-          .maxVars(2)
-          .maxValues(10)
-          .description("Variable(s) must have 10 or fewer unique values and be of the same or a parent entity as the Overlay variable.")
       .done();
   }
   
   @Override
-  protected void validateVisualizationSpec(ScatterplotSpec pluginSpec) throws ValidationException {
+  protected void validateVisualizationSpec(FloatingScatterplotSpec pluginSpec) throws ValidationException {
     validateInputs(new DataElementSet()
       .entity(pluginSpec.getOutputEntityId())
       .var("xAxisVariable", pluginSpec.getXAxisVariable())
       .var("yAxisVariable", pluginSpec.getYAxisVariable())
-      .var("overlayVariable", pluginSpec.getOverlayVariable())
-      .var("facetVariable", pluginSpec.getFacetVariable()));
+      .var("overlayVariable", pluginSpec.getOverlayVariable()));
     if (pluginSpec.getMaxAllowedDataPoints() != null && pluginSpec.getMaxAllowedDataPoints() <= 0) {
       throw new ValidationException("maxAllowedDataPoints must be a positive integer");
     }
   }
 
   @Override
-  protected List<StreamSpec> getRequestedStreams(ScatterplotSpec pluginSpec) {
+  protected List<StreamSpec> getRequestedStreams(FloatingScatterplotSpec pluginSpec) {
     return ListBuilder.asList(
       new StreamSpec(DEFAULT_SINGLE_STREAM_NAME, pluginSpec.getOutputEntityId())
         .addVar(pluginSpec.getXAxisVariable())
         .addVar(pluginSpec.getYAxisVariable())
-        .addVar(pluginSpec.getOverlayVariable())
-        .addVars(pluginSpec.getFacetVariable()));
+        .addVar(pluginSpec.getOverlayVariable()));
   }
 
   @Override
   protected void writeResults(OutputStream out, Map<String, InputStream> dataStreams) throws IOException {
     PluginUtil util = getUtil();
-    ScatterplotSpec spec = getPluginSpec();
+    FloatingScatterplotSpec spec = getPluginSpec();
     Map<String, VariableSpec> varMap = new HashMap<String, VariableSpec>();
     varMap.put("xAxis", spec.getXAxisVariable());
     varMap.put("yAxis", spec.getYAxisVariable());
     varMap.put("overlay", spec.getOverlayVariable());
-    varMap.put("facet1", util.getVariableSpecFromList(spec.getFacetVariable(), 0));
-    varMap.put("facet2", util.getVariableSpecFromList(spec.getFacetVariable(), 1));
     String valueSpec = spec.getValueSpec().getValue();
-    String showMissingness = spec.getShowMissingness() != null ? spec.getShowMissingness().getValue() : "noVariables";
-    String deprecatedShowMissingness = showMissingness.equals("FALSE") ? "noVariables" : showMissingness.equals("TRUE") ? "strataVariables" : showMissingness;
     String yVarType = util.getVariableType(spec.getYAxisVariable());
     
     if (yVarType.equals("DATE") && !valueSpec.equals("raw")) {
@@ -141,23 +113,22 @@ public class ScatterplotPlugin extends AbstractEmptyComputePlugin<ScatterplotPos
     RFileSetProcessor filesProcessor = new RFileSetProcessor(dataStreams)
       .add(DEFAULT_SINGLE_STREAM_NAME, 
         spec.getMaxAllowedDataPoints(), 
-        deprecatedShowMissingness, 
+        "noVariables", 
         nonStrataVarColNames, 
         (name, conn) ->
         conn.voidEval(util.getVoidEvalFreadCommand(name,
           spec.getXAxisVariable(),
           spec.getYAxisVariable(),
-          spec.getOverlayVariable(),
-          util.getVariableSpecFromList(spec.getFacetVariable(), 0),
-          util.getVariableSpecFromList(spec.getFacetVariable(), 1)))
+          spec.getOverlayVariable()))
       );
 
     useRConnectionWithProcessedRemoteFiles(Resources.RSERVE_URL, filesProcessor, connection -> {
       connection.voidEval(getVoidEvalVariableMetadataList(varMap));
       String cmd = 
-          "plot.data::scattergl(" + DEFAULT_SINGLE_STREAM_NAME + ", variables, '" + 
-              valueSpec + "', '" + 
-              deprecatedShowMissingness + "')";
+          "plot.data::scattergl(data=" + DEFAULT_SINGLE_STREAM_NAME + ", variables=variables, '" + 
+              "value=" + valueSpec + "', '" + 
+              "sampleSizes=FALSE, " +
+              "completeCases=FALSE, 'noVariables')";
       streamResult(connection, cmd, out);
     }); 
   }
