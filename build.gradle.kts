f533485686470f5dd623f9fa344c79c13aa02e7b
Ryan Doherty <tech@conical.org>
2023-03-25 16:50:31 -0400

    Reference new compute tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ repositories {
 
 // versions
 val coreLib       = "6.14.4"         // Container core lib version
-val edaCompute    = "1.4.3"          // EDA Compute version (used to pull in compute plugin RAML)
+val edaCompute    = "1.4.4"          // EDA Compute version (used to pull in compute plugin RAML)
 val edaCommon     = "10.5.1"         // EDA Common version
 val fgputil       = "2.12.6-jakarta" // FgpUtil version
 
