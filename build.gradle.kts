767ed0e23074b8bd0242abbc0e8bd1b4207182b9
Dan Galdi <dmgaldi@upenn.edu>
2024-02-28 13:46:14 -0500

    Use latest EdaCommon

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ repositories {
 // versions
 val coreLib       = "6.19.1"         // Container core lib version
 val edaCompute    = "2.5.7"          // EDA Compute version (used to pull in compute plugin RAML)
-val edaCommon     = "11.7.1"         // EDA Common version
+val edaCommon     = "11.7.2"         // EDA Common version
 val fgputil       = "2.12.9-jakarta" // FgpUtil version
 
 // use local EDA compute compiled schema if project exists, else use released version;
